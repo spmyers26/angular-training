47fbd52889f3cf975aa22e58cc0b2ef06ca97507
stephenmyers <spmyers26@gmail.com>
2024-03-25 12:22:29 -0500

    adds table of looped array data

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesPersonListComponent implements OnInit {
 
+  // create an arry of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kamar", "anupkumar@luv2code.com", 50000),
+    new SalesPerson("John", "Doe", "johndoe@luv2code.com", 40000),
+    new SalesPerson("Claire", "Murphy", "clairemurphy@luv2code.com", 90000),
+    new SalesPerson("Mai", "Truong", "maitruong@luv2code.com", 60000)
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
